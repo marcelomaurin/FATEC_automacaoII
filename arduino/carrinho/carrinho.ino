56a0481dce5de5442fa516082bc4710437a37fa8
marcelomaurin <marcelomaurinmartins@gmail.com>
2022-11-22 08:30:27 -0300

    versao inicial

--- arduino/carrinho/carrinho.ino
+++ arduino/carrinho/carrinho.ino
@@ -10,6 +10,49 @@
 #define INCLUDE_GAMEPAD_MODULE
 #include <DabbleESP32.h>
 
+//PINOUT
+// TB6612FNG H-Bridge Connections (both PWM inputs driven by GPIO 12)
+#define MTR_PWM     12
+#define LEFT_M0     15
+#define LEFT_M1     14
+#define RIGHT_M0    13
+#define RIGHT_M1    2
+
+
+//Setting Motor PWM properties
+const int freq = 2000;
+const int motorPWMChannnel = 8;
+const int lresolution = 8;
+
+volatile unsigned int  motor_speed   = 200;
+volatile unsigned long previous_time = 0;
+volatile unsigned long move_interval = 250;
+
+//chamadas de funçoes
+void robot_stop();
+void robot_fwd();
+void robot_back();
+void robot_right();
+void robot_left();
+void update_speed();
+unsigned int get_speed(unsigned int sp);
+
+
+void Start_PonteH()
+{
+    // Pins for Motor Controller
+    pinMode(LEFT_M0,OUTPUT);
+    pinMode(LEFT_M1,OUTPUT);
+    pinMode(RIGHT_M0,OUTPUT);
+    pinMode(RIGHT_M1,OUTPUT);
+    robot_stop();
+    
+     // Motor uses PWM Channel 8
+    ledcAttachPin(MTR_PWM, 8);
+    ledcSetup(8, 2000, 8);      
+    ledcWrite(8, 130);
+}
+
 void Start_Serial()
 {
    
@@ -25,36 +68,102 @@ void setup()
 {
  Start_Serial();
  Start_Dabble();
+ Start_PonteH();
 }
 
+void update_speed()
+{  
+    ledcWrite(motorPWMChannnel, get_speed(motor_speed));
+    
+}
+
+unsigned int get_speed(unsigned int sp)
+{
+  return map(sp, 0, 100, 0, 255);
+}
+ 
+
+
+void robot_stop()
+{
+  digitalWrite(LEFT_M0,LOW);
+  digitalWrite(LEFT_M1,LOW);
+  digitalWrite(RIGHT_M0,LOW);
+  digitalWrite(RIGHT_M1,LOW);
+}
+ 
+void robot_fwd()
+{
+  digitalWrite(LEFT_M0,HIGH);
+  digitalWrite(LEFT_M1,LOW);
+  digitalWrite(RIGHT_M0,HIGH);
+  digitalWrite(RIGHT_M1,LOW);
+  move_interval=250;
+  previous_time = millis();  
+}
+ 
+void robot_back()
+{
+  digitalWrite(LEFT_M0,LOW);
+  digitalWrite(LEFT_M1,HIGH);
+  digitalWrite(RIGHT_M0,LOW);
+  digitalWrite(RIGHT_M1,HIGH);
+  move_interval=250;
+   previous_time = millis();  
+}
+ 
+void robot_right()
+{
+  digitalWrite(LEFT_M0,LOW);
+  digitalWrite(LEFT_M1,HIGH);
+  digitalWrite(RIGHT_M0,HIGH);
+  digitalWrite(RIGHT_M1,LOW);
+  move_interval=100;
+   previous_time = millis();
+}
+ 
+void robot_left()
+{
+  digitalWrite(LEFT_M0,HIGH);
+  digitalWrite(LEFT_M1,LOW);
+  digitalWrite(RIGHT_M0,LOW);
+  digitalWrite(RIGHT_M1,HIGH);
+  move_interval=100;
+   previous_time = millis();
+}
 
 void Le_Dable()
 {
-    Dabble.processInput();             //this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
+  Dabble.processInput();             //this function is used to refresh data obtained from smartphone.Hence calling this function is mandatory in order to get data properly from your mobile.
   Serial.print("KeyPressed: ");
   if (GamePad.isUpPressed())
   {
     Serial.print("Up");
+    robot_fwd();
   }
 
   if (GamePad.isDownPressed())
   {
     Serial.print("Down");
+    robot_back();
   }
 
   if (GamePad.isLeftPressed())
   {
     Serial.print("Left");
+    robot_left();
   }
 
   if (GamePad.isRightPressed())
   {
     Serial.print("Right");
+    robot_right();
   }
 
   if (GamePad.isSquarePressed())
   {
     Serial.print("Square");
+    robot_stop();
   }
 
   if (GamePad.isCirclePressed())
@@ -75,6 +184,7 @@ void Le_Dable()
   if (GamePad.isStartPressed())
   {
     Serial.print("Start");
+    robot_stop();
   }
 
   if (GamePad.isSelectPressed())
